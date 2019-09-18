4934e3822f5487c0997d85edb4344faf81702935
KaiKai <jdirlcmhkd0812@gmail.com>
2019-09-18 23:11:57 +0800

    update

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def handle_message(event):
         r = '怎麼又有人已讀～'
     elif '彥' in msg:
         r = '彥丞該戒煙了'
-    elif ['股票', '石油', '退休'] in msg:
+    elif msg in ['股票', '石油', '退休']:
         r = '要起飛了嗎'
     elif '敦' in msg:
         r = '董事長好'
